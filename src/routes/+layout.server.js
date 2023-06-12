4e338e3c2b01b5a5af2096304dba9b0f3fe61573
Rob Leroy <robleroy@msn.com>
2023-06-12 12:05:09 -0500

    update

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -19,21 +19,13 @@ export async function load(event) {
     
   } else {
     ipData = {
-      ip: "107.207.41.105",
-      city: "Oklahoma City",
-      region: "OK",
-      country: "US",
-      lat: 35.4662,
-      lon: -97.5168,
-    };
-    // ipData = {
-    //   ip:      event.getClientAddress(),
-    //   city:    decodeURIComponent(event.request.headers.get('x-vercel-ip-city') ?? 'unknown'),
-    //   region:  decodeURIComponent(event.request.headers.get('x-vercel-ip-country-region') ?? 'unknown'),
-    //   country: decodeURIComponent(event.request.headers.get('x-vercel-ip-country') ?? 'unknown'),
-    //   lat:     decodeURIComponent(event.request.headers.get('x-vercel-ip-latitude') ?? 'unknown'),
-    //   lon:     decodeURIComponent(event.request.headers.get('x-vercel-ip-longitude') ?? 'unknown'),
-    // }
+      ip:      event.getClientAddress(),
+      city:    decodeURIComponent(event.request.headers.get('x-vercel-ip-city') ?? 'unknown'),
+      region:  decodeURIComponent(event.request.headers.get('x-vercel-ip-country-region') ?? 'unknown'),
+      country: decodeURIComponent(event.request.headers.get('x-vercel-ip-country') ?? 'unknown'),
+      lat:     decodeURIComponent(event.request.headers.get('x-vercel-ip-latitude') ?? 'unknown'),
+      lon:     decodeURIComponent(event.request.headers.get('x-vercel-ip-longitude') ?? 'unknown'),
+    }
   }
   
 
